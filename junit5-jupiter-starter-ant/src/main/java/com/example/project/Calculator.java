860e50c6b9fccb86013a143a30d6c82a329732b0
Shay <60371258+apherios@users.noreply.github.com>
2020-03-30 17:28:42 -0500

    Update Calculator.java

--- junit5-jupiter-starter-ant/src/main/java/com/example/project/Calculator.java
+++ junit5-jupiter-starter-ant/src/main/java/com/example/project/Calculator.java
@@ -10,10 +10,18 @@
 
 package com.example.project;
 
-public class Calculator {
+public class Calculator 
+{
 
-	public int add(int a, int b) {
+	public int add(int a, int b) 
+	{
 		return a + b;
+	}
+
+	
+	public int sub(int a, int b) 
+	{
+		return a - b;
 	}
 
 }
